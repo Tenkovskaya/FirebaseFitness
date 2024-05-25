5c3cdf46c469d507fca3403ec4fbc753f1414ead
AliAli01010101 <at@evamobi.com>
2024-05-25 22:16:25 +0300

    new

--- app/src/main/java/com/tenkovskaya/fitnes/domain/repository/UserRepository.kt
+++ app/src/main/java/com/tenkovskaya/fitnes/domain/repository/UserRepository.kt
@@ -3,5 +3,4 @@ package com.tenkovskaya.fitnes.domain.repository
 interface UserRepository {
     suspend fun register(email: String, password: String): Boolean
     suspend fun login(email: String, password: String): Boolean
-
 }
